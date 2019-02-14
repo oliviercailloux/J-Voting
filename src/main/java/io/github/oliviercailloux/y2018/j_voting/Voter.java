a4b64cd98b9e1a2fcaef137c206fba2a388199f7
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2019-02-14 14:05:05 +0100

    ToString voter

--- src/main/java/io/github/oliviercailloux/y2018/j_voting/Voter.java
+++ src/main/java/io/github/oliviercailloux/y2018/j_voting/Voter.java
@@ -3,6 +3,7 @@ package io.github.oliviercailloux.y2018.j_voting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 
 /**
@@ -14,9 +15,8 @@ public class Voter implements Comparable<Voter> {
 
 	/**
 	 * Creates a new object Voter with the id given as a parameter
-	 * 
-	 * @param id
-	 *            int <code>not null</code>
+	 *
+	 * @param id int <code>not null</code>
 	 */
 	public Voter(int id) {
 		this.id = Preconditions.checkNotNull(id);
@@ -30,8 +30,22 @@ public class Voter implements Comparable<Voter> {
 	}
 
 	/**
-	 * @param voter
-	 *            <code> not null</code>
+	 *
+	 *
+	 * @param v2 not <code> null </code>
+	 * @return an integer : 0 if the voters have the same id, <0 if the calling
+	 *         voter is smaller than the parameter, else >0.
+	 */
+	@Override
+	public int compareTo(Voter v2) {
+		LOGGER.debug("compare:");
+		Preconditions.checkNotNull(v2);
+		LOGGER.debug("calling voter : v1 {}, parameter v2 {}", this.getId(), v2.getId());
+		return this.getId() - v2.getId();
+	}
+
+	/**
+	 * @param voter <code> not null</code>
 	 * @return whether two voters are equal, ie have the same id.
 	 */
 	@Override
@@ -50,19 +64,8 @@ public class Voter implements Comparable<Voter> {
 		return id;
 	}
 
-	/**
-	 * 
-	 * 
-	 * @param v2
-	 *            not <code> null </code>
-	 * @return an integer : 0 if the voters have the same id, <0 if the calling
-	 *         voter is smaller than the parameter, else >0.
-	 */
 	@Override
-	public int compareTo(Voter v2) {
-		LOGGER.debug("compare:");
-		Preconditions.checkNotNull(v2);
-		LOGGER.debug("calling voter : v1 {}, parameter v2 {}", this.getId(), v2.getId());
-		return this.getId() - v2.getId();
+	public String toString() {
+		return MoreObjects.toStringHelper(this).addValue(id).toString();
 	}
 }
