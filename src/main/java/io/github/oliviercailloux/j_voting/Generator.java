5ff43af87577922745db403810d4b1ed2f88679f
ErnestBidouille <theo.chennebault@gmail.com>
2019-05-18 00:23:13 +0200

    Task 5#  Rename all packages

---
+++ src/main/java/io/github/oliviercailloux/j_voting/Generator.java
@@ -0,0 +1,23 @@
+package io.github.oliviercailloux.j_voting;
+
+import com.google.common.collect.ImmutableSet;
+
+public class Generator {
+
+    public static ImmutableSet<Alternative> getAlternatives(int m) {
+        final ImmutableSet.Builder<Alternative> builder = ImmutableSet
+                        .builder();
+        for (int i = 1; i <= m; ++i) {
+            builder.add(new Alternative(i));
+        }
+        return builder.build();
+    }
+
+    public static ImmutableSet<Voter> getVoters(int n) {
+        final ImmutableSet.Builder<Voter> builder = ImmutableSet.builder();
+        for (int i = 1; i <= n; ++i) {
+            builder.add(new Voter(i));
+        }
+        return builder.build();
+    }
+}
---
+++ src/test/java/io/github/oliviercailloux/j_voting/AlternativeTest.java
@@ -0,0 +1,41 @@
+package io.github.oliviercailloux.j_voting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.oliviercailloux.j_voting.Alternative;
+
+public class AlternativeTest {
+
+    /**
+     * Tests whether method getId() returns the value of the id declared at the
+     * creation of this Alternative
+     */
+    @Test
+    public void testGetId() {
+        Alternative a = new Alternative(7);
+        assertEquals(a.getId(), 7);
+    }
+
+    /**
+     * Tests whether method equals() returns true for two Alternatives having
+     * the same id
+     */
+    @Test
+    public void testEqualsAlternative() {
+        Alternative a = new Alternative(7);
+        Alternative b = new Alternative(7);
+        assertEquals(a, b);
+    }
+
+    /**
+     * Tests whether method toString() returns the id declared at the creation
+     * of this Alternative
+     */
+    @Test
+    public void testToString() {
+        Alternative a = new Alternative(7);
+        assertEquals(a.toString(), Integer.toString(7));
+    }
+}
