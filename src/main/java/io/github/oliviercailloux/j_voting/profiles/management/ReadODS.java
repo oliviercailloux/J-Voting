75b38da031c6849061a0a3cbe30ed4a0ce2c2ff3
ErnestBidouille <theo.chennebault@gmail.com>
2019-05-24 16:37:53 +0200

    Changes in ReadODS.java

--- src/main/java/io/github/oliviercailloux/j_voting/profiles/management/ReadODS.java
+++ src/main/java/io/github/oliviercailloux/j_voting/profiles/management/ReadODS.java
@@ -2,9 +2,7 @@ package io.github.oliviercailloux.j_voting.profiles.management;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.CellRange;
@@ -23,64 +21,74 @@ public class ReadODS {
 
     private static final Logger LOGGER = LoggerFactory
                     .getLogger(ReadODS.class.getName());
-    private static Table sheet;
+    private static Table table;
 
     public static void main(String[] args) throws Exception {
-        checkFormat();
+        checkFormat("./testods.ods");
+        checkFormat("./facon1.ods");
+        checkFormat("./facon2.ods");
     }
 
-    public static void checkFormat() throws Exception {
-        String ok = "./testods.ods";
-        InputStream inputStream = ReadODS.class.getResourceAsStream(ok);
+    public static void checkFormat(String inputString) throws Exception {
+        InputStream inputStream = ReadODS.class
+                        .getResourceAsStream(inputString);
         LOGGER.debug("Open Stream");
         SpreadsheetDocument spreadsheetDoc = SpreadsheetDocument
                         .loadDocument(inputStream);
         LOGGER.debug("Open Spreadsheet");
-        sheet = spreadsheetDoc.getSheetByIndex(0);
+        table = spreadsheetDoc.getSheetByIndex(0);
         LOGGER.debug("Get sheet index 0 done");
-        if (sheet.getCellByPosition(1, 0).getStringValue().equals(""))
+        if (table.getCellByPosition(1, 0).getStringValue().equals(""))
             readSpreadsheetDocument();
-        else if (sheet.getCellByPosition(0, 0).getStringValue().equals(""))
+        else if (table.getCellByPosition(0, 0).getStringValue().equals(""))
             readFormat1();
         else
             readFormat2();
     }
 
     public static void readSpreadsheetDocument() throws Exception {
-        int nbCandidates = Integer.parseInt(
-                        sheet.getCellByPosition(0, 0).getStringValue());
-        List<Alternative> alter = new ArrayList<>(nbCandidates);
-        for (int i = 1; i <= nbCandidates; i++)
-            alter.add(new Alternative(i));
-        System.out.println(nbCandidates + " candidates");
-        System.out.println("List of candidates : " + alter);
+        StringBuilder stringBuilder = new StringBuilder();
+        int nbAlternatives = Integer.parseInt(
+                        table.getCellByPosition(0, 0).getStringValue());
+        List<Alternative> alternatives = new ArrayList<>(nbAlternatives);
+        for (int i = 1; i <= nbAlternatives; i++)
+            alternatives.add(new Alternative(i));
+        stringBuilder.append("There are " + nbAlternatives + " alternatives\n"
+                        + "List of alternatives : " + alternatives + "\n");
         int nbOrders = Integer
-                        .parseInt(sheet.getCellByPosition(2, nbCandidates + 1)
+                        .parseInt(table.getCellByPosition(2, nbAlternatives + 1)
                                         .getStringValue());
-        Set<Alternative> pref = new LinkedHashSet<>(nbCandidates);
-        System.out.println(nbOrders + " differents orders");
-        CellRange prefRange = sheet.getCellRangeByPosition(1, nbCandidates + 2,
-                        nbCandidates, nbOrders + nbCandidates + 2);
-        List<Alternative> ordersList = new ArrayList<Alternative>(nbOrders);
-        for (int i = 0; i < prefRange.getRowNumber() - 1; i++) {
+        // Set<Alternative> pref = new LinkedHashSet<>(nbCandidates);
+        stringBuilder.append("There are " + nbOrders + " differents orders\n");
+        CellRange prefRange = table.getCellRangeByPosition(1,
+                        nbAlternatives + 2, nbAlternatives,
+                        nbOrders + nbAlternatives + 1);
+        // List<Alternative> ordersList = new ArrayList<>(nbOrders);
+        for (int i = 0; i < prefRange.getRowNumber(); i++) {
             int nbVoters = Integer.parseInt(
-                            sheet.getCellByPosition(0, i + nbCandidates + 2)
+                            table.getCellByPosition(0, i + nbAlternatives + 2)
                                             .getStringValue());
-            System.out.print("\n" + nbVoters + " voters for preference " + i
+            stringBuilder.append(nbVoters + " voters for preference " + (i + 1)
                             + " : ");
             for (int j = 0; j < prefRange.getColumnNumber(); j++) {
-                ordersList.add(alter.get(Integer.valueOf(prefRange
-                                .getCellByPosition(j, i).getStringValue())
-                                - 1));
-                System.out.print(prefRange.getCellByPosition(j, i)
-                                .getStringValue() + " > ");
+                // ordersList.add(alternatives.get(Integer.valueOf(prefRange
+                // .getCellByPosition(j, i).getStringValue())
+                // - 1));
+                stringBuilder.append(prefRange.getCellByPosition(j, i)
+                                .getStringValue() + ">");
             }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1).append("\n");
         }
+        System.out.println(stringBuilder);
     }
 
     public static void readFormat1() throws Exception {
+        System.out.println("1");
+        // TODO
     }
 
     public static void readFormat2() throws Exception {
+        // TODO
+        System.out.println("2");
     }
 }
