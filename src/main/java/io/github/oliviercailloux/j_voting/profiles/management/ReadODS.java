a6c2f54c795ed42eb9a04d71ac72eea3844e4bc0
emiliechhean <emilie.chhean@gmail.com>
2019-05-26 17:32:21 +0200

    2# ODSProfile : Add methods in order to read 2 new format types of .ODS

--- src/main/java/io/github/oliviercailloux/j_voting/profiles/management/ReadODS.java
+++ src/main/java/io/github/oliviercailloux/j_voting/profiles/management/ReadODS.java
@@ -2,7 +2,9 @@ package io.github.oliviercailloux.j_voting.profiles.management;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.CellRange;
@@ -25,8 +27,10 @@ public class ReadODS {
     public static void main(String[] args) throws Exception {
         System.out.println(checkFormat(
                         ReadODS.class.getResourceAsStream("./testods.ods")));
-        checkFormat(ReadODS.class.getResourceAsStream("./facon1.ods"));
-        checkFormat(ReadODS.class.getResourceAsStream("./facon2.ods"));
+        System.out.println(checkFormat(
+                        ReadODS.class.getResourceAsStream("./facon1.ods")));
+        System.out.println(checkFormat(
+                        ReadODS.class.getResourceAsStream("./facon2.ods")));
     }
 
     public static String checkFormat(InputStream inputStream) throws Exception {
@@ -56,7 +60,7 @@ public class ReadODS {
                         .parseInt(table.getCellByPosition(2, nbAlternatives + 1)
                                         .getStringValue());
         // Set<Alternative> pref = new LinkedHashSet<>(nbCandidates);
-        stringBuilder.append("There are " + nbOrders + " differents orders\n");
+        stringBuilder.append("There are " + nbOrders + " different orders\n");
         CellRange prefRange = table.getCellRangeByPosition(1,
                         nbAlternatives + 2, nbAlternatives,
                         nbOrders + nbAlternatives + 1);
@@ -80,14 +84,92 @@ public class ReadODS {
     }
 
     public static String readFormat1(Table table) throws Exception {
-        System.out.println("1");
         // TODO
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        int nbAlternatives = getNbAlternatives(table);
+        int nbTotVoters = getnbTotVoters(table, 1);
+        CellRange prefRange = table.getCellRangeByPosition(1, 1, nbTotVoters,
+                        nbAlternatives);
+        for (int j = 0; j < prefRange.getColumnNumber(); j++) {
+            int max = Integer.parseInt(
+                            prefRange.getCellByPosition(j, 0).getStringValue());
+            for (int i = 0; i < prefRange.getRowNumber(); i++) {
+                if (max < Integer.parseInt(prefRange.getCellByPosition(j, i)
+                                .getStringValue()))
+                    max = Integer.parseInt(prefRange.getCellByPosition(j, i)
+                                    .getStringValue());
+            }
+            stringBuilder.append("Voter " + (j + 1) + " : ");
+            int nbChoice = 1;
+            Set<Alternative> set = new LinkedHashSet<>();
+            while (nbChoice <= max) {
+                set = new LinkedHashSet<>();
+                for (int i = 0; i < prefRange.getRowNumber(); i++) {
+                    if (nbChoice == Integer
+                                    .parseInt(prefRange.getCellByPosition(j, i)
+                                                    .getStringValue())) {
+                        int number = Integer.parseInt(
+                                        table.getCellByPosition(0, i + 1)
+                                                        .getStringValue());
+                        set.add(new Alternative(number));
+                    }
+                }
+                nbChoice++;
+                if (set.size() >= 2) {
+                    stringBuilder.append("{");
+                    for (Alternative alter : set) {
+                        stringBuilder.append(alter + ",");
+                    }
+                    stringBuilder.deleteCharAt(stringBuilder.length() - 1)
+                                    .append("}>");
+                } else {
+                    for (Alternative alter : set) {
+                        stringBuilder.append(alter + ">");
+                    }
+                }
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1).append("\n");
+        }
+        return stringBuilder.toString();
     }
 
     public static String readFormat2(Table table) throws Exception {
-        // TODO
-        System.out.println("2");
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        int nbAlternatives = getNbAlternatives(table);
+        int nbTotVoters = getnbTotVoters(table, 0);
+        CellRange prefRange = table.getCellRangeByPosition(0, 1,
+                        nbTotVoters - 1, nbAlternatives);
+        for (int j = 0; j < prefRange.getColumnNumber(); j++) {
+            stringBuilder.append("Voter " + (j + 1) + " : ");
+            for (int i = 0; i < prefRange.getRowNumber(); i++) {
+                stringBuilder.append(Integer.parseInt(prefRange
+                                .getCellByPosition(j, i).getStringValue())
+                                + ">");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private static int getNbAlternatives(Table table) {
+        int nbAlternatives = 0;
+        List<Alternative> alternatives = new ArrayList<>(nbAlternatives);
+        while (!table.getCellByPosition(0, nbAlternatives + 1).getStringValue()
+                        .equals("")) {
+            nbAlternatives++;
+            alternatives.add(new Alternative(nbAlternatives));
+        }
+        System.out.println("There are " + nbAlternatives + " alternatives\n"
+                        + "List of alternatives : " + alternatives);
+        return nbAlternatives;
+    }
+
+    private static int getnbTotVoters(Table table, int position) {
+        int nbTotVoters = 0;
+        while (!table.getCellByPosition(nbTotVoters + position, 0)
+                        .getStringValue().equals(""))
+            nbTotVoters++;
+        System.out.println(("There are " + nbTotVoters + " voters"));
+        return nbTotVoters;
     }
 }
