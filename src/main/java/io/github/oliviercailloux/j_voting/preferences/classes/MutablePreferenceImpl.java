51442472e8aa78fea1dce59de64c708a1c8d8d95
matteoloubet <30loubet@gmail.com>
2019-06-09 19:24:11 +0200

    + Completed MutablePreferenceImpl class implementing MutablePreference interface +--> Added some javadoc +--> completed some methods

--- src/main/java/io/github/oliviercailloux/j_voting/preferences/classes/MutablePreferenceImpl.java
+++ src/main/java/io/github/oliviercailloux/j_voting/preferences/classes/MutablePreferenceImpl.java
@@ -1,13 +1,15 @@
 package io.github.oliviercailloux.j_voting.preferences.classes;
 
-import com.google.common.graph.Graph;
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
 import io.github.oliviercailloux.j_voting.Alternative;
 import io.github.oliviercailloux.j_voting.Voter;
 import io.github.oliviercailloux.j_voting.preferences.interfaces.MutablePreference;
 import io.github.oliviercailloux.j_voting.preferences.interfaces.Preference;
-import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -26,14 +28,16 @@ public class MutablePreferenceImpl implements MutablePreference {
     
     protected MutableGraph<Alternative> graph;
     protected Voter voter;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MutablePreference.class.getName());
     
-    public MutablePreferenceImpl(Graph<Alternative> G, Voter V) {
-        graph = GraphBuilder.from(G).build();
-        voter = V;
+    
+    private MutablePreferenceImpl(Preference pref) {
+        graph = GraphBuilder.from(pref.asGraph()).build();
+        voter = pref.getVoter();
     }
     
-    private MutablePreferenceImpl(Set<List<Set<Alternative>>> pref, Voter V) {
-        voter = V;
+    private MutablePreferenceImpl(Set<List<Set<Alternative>>> pref, Voter voter) {
+        this.voter = voter;
         graph = GraphBuilder.directed().allowsSelfLoops(true).build();
         for (List<Set<Alternative>> array : pref) {
             ArrayList<Alternative> tmp = new ArrayList<>();
@@ -46,7 +50,6 @@ public class MutablePreferenceImpl implements MutablePreference {
                         graph.addNode(alt);
                     tmp.add(alt);
                 }
-                
                 // then create edges from every node in TMP to every node in current equality set
                 for (Alternative alt : tmp) {
                     for (Alternative alt2 : set) {
@@ -57,20 +60,53 @@ public class MutablePreferenceImpl implements MutablePreference {
         }
     }
     
-    public static MutablePreferenceImpl of(Set<List<Set<Alternative>>> S, Voter V) {
-        return new MutablePreferenceImpl(S, V);
+    /**
+     * Static factory method creating a mutable preference from a setAlternatives of data. Those datas are implemented in a graph.
+     *
+     *
+     * @param setAlternatives is a setAlternatives of lists of sets of Alternative representing the preference.
+     *          In the first setAlternatives, every list is a linear comparison of sets of alternatives. (first in the least is preferred to next ones, etc.)
+     *          Those sets of alternatives contain ex-aequo alternatives.
+     * @param voter is the Voter associated to the Preference.
+     * @return the mutable preference, implemented with a graph.
+     * @see Voter
+     * @see Preference
+     * @see MutablePreference#asGraph()
+     */
+    public static MutablePreferenceImpl of(Set<List<Set<Alternative>>> setAlternatives, Voter voter) {
+        LOGGER.debug("MutablePreferenceImpl of Factory");
+        Preconditions.checkNotNull(setAlternatives);
+        Preconditions.checkNotNull(voter);
+        return new MutablePreferenceImpl(setAlternatives, voter);
     }
     
+    public static MutablePreferenceImpl of(Preference pref) {
+        Preconditions.checkNotNull(pref);
+        return new MutablePreferenceImpl(pref);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
     @Override public MutableGraph<Alternative> asGraph() {
         return graph;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override public Set<Alternative> getAlternatives() {
-        return null;
+        return graph.nodes();
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override public Voter getVoter() {
         return voter;
     }
     
+    @Override public String toString() {
+        return graph.toString() + "\n" + voter.toString();
+    }
 }
