a8e998c429d9a36025b1555c9b3e88e83e1c4fd4
emiliechhean <emilie.chhean@gmail.com>
2019-06-20 00:54:02 +0200

    Edit Javadoc

--- src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/MutablePreference.java
+++ src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/MutablePreference.java
@@ -2,31 +2,33 @@ package io.github.oliviercailloux.j_voting.preferences.interfaces;
 
 import com.google.common.graph.ImmutableGraph;
 import com.google.common.graph.MutableGraph;
+
 import io.github.oliviercailloux.j_voting.Alternative;
 
 /**
  * A mutable preference is a preference in which some alternatives can be added,
  * which means some edges (pair of alternatives) can be added.
- * <p>
+ * 
  * If an alternative a is strictly better than an alternative b, it is
  * impossible to change this afterwards.
  */
 public interface MutablePreference extends Preference {
-    
-    /**
-     * {@inheritDoc}
-     * <p>
-     * The returned graph can't be modified, though is not necessarily immutable.
-     *
-     * @see MutablePreference#asMutableGraph()
-     */
-    @Override public ImmutableGraph<Alternative> asGraph();
-    
-    /**
-     * This graph is not necessarily transitively closed and not necessarily reflexive
-     *
-     * @see MutablePreference#asGraph()
-     *  TODO : should forbid removing alternatives from the set of nodes (surprising results when a > b > c and removing b as it might disconnect the graph)
-     */
-    public MutableGraph<Alternative> asMutableGraph();
+
+	/**
+	 * The returned graph can't be modified, though is not necessarily immutable.
+	 *
+	 * @see MutablePreference#asMutableGraph()
+	 */
+	@Override
+	public ImmutableGraph<Alternative> asGraph();
+
+	/**
+	 * This graph is not necessarily transitively closed and not necessarily
+	 * reflexive
+	 *
+	 * @see MutablePreference#asGraph() TODO : should forbid removing alternatives
+	 *      from the set of nodes (surprising results when a > b > c and removing b
+	 *      as it might disconnect the graph)
+	 */
+	public MutableGraph<Alternative> asMutableGraph();
 }
