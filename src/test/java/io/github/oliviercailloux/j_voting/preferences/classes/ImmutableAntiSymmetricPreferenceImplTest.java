a6d5e509b603b4b78eebd097460031163427b083
ErnestBidouille <theo.chennebault@gmail.com>
2019-06-24 20:23:36 +0200

    minor change

---
+++ src/test/java/io/github/oliviercailloux/j_voting/preferences/classes/ImmutableAntiSymmetricPreferenceImplTest.java
@@ -0,0 +1,29 @@
+package io.github.oliviercailloux.j_voting.preferences.classes;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
+
+import io.github.oliviercailloux.j_voting.Alternative;
+import io.github.oliviercailloux.j_voting.Voter;
+
+class ImmutableAntiSymmetricPreferenceImplTest {
+
+    @Test
+    void asImmutableAntisymmetricPreferenceException() {
+        MutableGraph<Alternative> graph = GraphBuilder.directed()
+                        .allowsSelfLoops(true).build();
+        graph.putEdge(Alternative.withId(1), Alternative.withId(1));
+        graph.putEdge(Alternative.withId(4), Alternative.withId(4));
+        graph.putEdge(Alternative.withId(1), Alternative.withId(4));
+        graph.putEdge(Alternative.withId(4), Alternative.withId(1));
+        assertThrows(IllegalArgumentException.class,
+                        () -> ImmutableAntiSymmetricPreferenceImpl
+                                        .asImmutableAntiSymmetricPreference(
+                                                        Voter.createVoter(2),
+                                                        graph));
+    }
+}
