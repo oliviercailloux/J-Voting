0f7542ab00375a92221a6c1572e739bfbea2cebf
Anael Dray <anaeldray@hotmail.fr>
2019-06-26 23:07:55 +0200

    minor changes

--- src/test/java/io/github/oliviercailloux/j_voting/preferences/MutablePreferenceImplTest.java
+++ src/test/java/io/github/oliviercailloux/j_voting/preferences/MutablePreferenceImplTest.java
@@ -305,31 +305,4 @@ class MutablePreferenceImplTest {
         returnedSet.add(a5);
         assertEquals(returnedSet, pref.getAlternatives());
     }
-
-    /**
-     * Tests whether method getAlternatives modifies the corresponding graph if
-     * the alternatives set is modified.
-     */
-    @Test
-    void testGetAlternatives1() {
-        Alternative a1 = Alternative.withId(1);
-        Alternative a2 = Alternative.withId(2);
-        Alternative a3 = Alternative.withId(3);
-        Alternative a4 = Alternative.withId(4);
-        Alternative a5 = Alternative.withId(5);
-        Alternative a6 = Alternative.withId(6);
-        Set<Alternative> A = Sets.newHashSet(a1, a2);
-        Set<Alternative> B = Sets.newHashSet(a3, a4);
-        Set<Alternative> C = Sets.newHashSet(a5);
-        ArrayList<Set<Alternative>> listTest = Lists.newArrayList(A, B, C);
-        Set<List<Set<Alternative>>> setTest = Sets.newHashSet();
-        setTest.add(listTest);
-        MutablePreferenceImpl pref = MutablePreferenceImpl.given(
-                        MutablePreferenceImpl.preferenceGraphMaker(setTest),
-                        Voter.createVoter(1));
-        Set<Alternative> a = Set.copyOf(pref.getAlternatives());
-        pref.addAlternative(a6);
-        a.add(a6);
-        assertEquals(a, pref.getAlternatives());
-    }
 }
