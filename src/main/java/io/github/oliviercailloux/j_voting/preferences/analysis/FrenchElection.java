124f14eb911e3e13b6d251d25fc0fdd0cc8ed56f
ErnestBidouille <theo.chennebault@gmail.com>
2019-06-29 16:29:34 +0200

    Some changes

--- src/main/java/io/github/oliviercailloux/j_voting/preferences/analysis/FrenchElection.java
+++ src/main/java/io/github/oliviercailloux/j_voting/preferences/analysis/FrenchElection.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,10 +20,10 @@ import io.github.oliviercailloux.j_voting.preferences.interfaces.LinearPreferenc
 
 public class FrenchElection {
 
-    ImmutableSet<Alternative> setAlternatives;
-    ImmutableSet<Voter> setVoters;
-    Map<Alternative, Integer> scores;
-    Alternative winner;
+    private ImmutableSet<Alternative> alternatives;
+    private ImmutableSet<Voter> voters;
+    private Map<Alternative, Integer> scores;
+    private ImmutableSet<Alternative> winners;
     private static final Logger LOGGER = LoggerFactory
                     .getLogger(FrenchElection.class.getName());
 
@@ -51,30 +52,33 @@ public class FrenchElection {
             if (!tmpVoters.add(linearPreference.getVoter()))
                 throw new IllegalArgumentException(
                                 "A voter can't vote two times in a same election");
+            for (Alternative alternative : linearPreference.getAlternatives())
+                tmpScores.putIfAbsent(alternative, 0);
             Alternative tmpAlternative = linearPreference.asList().get(0);
             Integer votes = (tmpScores.get(tmpAlternative) != null)
                             ? tmpScores.get(tmpAlternative)
                             : 0;
             tmpScores.put(tmpAlternative, votes + 1);
         }
-        Entry<Alternative, Integer> bestAlternative = Collections.max(
-                        tmpScores.entrySet(),
-                        (Entry<Alternative, Integer> e1,
+        this.winners = ImmutableSet.copyOf(tmpScores.entrySet().stream()
+                        .filter(entry -> Collections.max(tmpScores.entrySet(), (
+                                        Entry<Alternative, Integer> e1,
                                         Entry<Alternative, Integer> e2) -> e1
                                                         .getValue()
-                                                        .compareTo(e2.getValue()));
-        this.setAlternatives = ImmutableSet.copyOf(tmpScores.keySet());
-        this.winner = bestAlternative.getKey();
+                                                        .compareTo(e2.getValue()))
+                                        .getValue().equals(entry.getValue()))
+                        .map(Map.Entry::getKey).collect(Collectors.toSet()));
         this.scores = tmpScores;
-        this.setVoters = ImmutableSet.copyOf(tmpVoters);
+        this.alternatives = ImmutableSet.copyOf(tmpScores.keySet());
+        this.voters = ImmutableSet.copyOf(tmpVoters);
     }
 
-    public Alternative getWinner() {
-        return winner;
+    public ImmutableSet<Alternative> getWinners() {
+        return winners;
     }
 
-    public ImmutableSet<Alternative> getListAlternatives() {
-        return setAlternatives;
+    public ImmutableSet<Alternative> getAlternatives() {
+        return alternatives;
     }
 
     public Map<Alternative, Integer> getScores() {
@@ -82,6 +86,6 @@ public class FrenchElection {
     }
 
     public ImmutableSet<Voter> getVoters() {
-        return setVoters;
+        return voters;
     }
 }
--- src/test/java/io/github/oliviercailloux/j_voting/preferences/analysis/FrenchElectionTest.java
+++ src/test/java/io/github/oliviercailloux/j_voting/preferences/analysis/FrenchElectionTest.java
@@ -3,6 +3,7 @@ package io.github.oliviercailloux.j_voting.preferences.analysis;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a1;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a2;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a3;
+import static io.github.oliviercailloux.j_voting.AlternativeHelper.a4;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -31,10 +32,20 @@ class FrenchElectionTest {
     void testWinner() throws EmptySetException, DuplicateValueException {
         FrenchElection toTest = FrenchElection.asFrenchElection(ImmutableSet.of(
                         getLinearPref(Voter.createVoter(1), a1, a3, a2),
-                        getLinearPref(Voter.createVoter(2), a2, a3, a1),
+                        getLinearPref(Voter.createVoter(2), a2, a3),
                         getLinearPref(Voter.createVoter(3), a3, a1, a2),
                         getLinearPref(Voter.createVoter(4), a2, a3, a1)));
-        assertEquals(a2, toTest.winner);
+        assertEquals(ImmutableSet.of(a2), toTest.getWinners());
+    }
+
+    @Test
+    void testTwoWinner() throws EmptySetException, DuplicateValueException {
+        FrenchElection toTest = FrenchElection.asFrenchElection(ImmutableSet.of(
+                        getLinearPref(Voter.createVoter(1), a1, a3, a2),
+                        getLinearPref(Voter.createVoter(2), a2, a3),
+                        getLinearPref(Voter.createVoter(3), a1, a3, a2),
+                        getLinearPref(Voter.createVoter(4), a2, a3, a1)));
+        assertEquals(ImmutableSet.of(a2, a1), toTest.getWinners());
     }
 
     @Test
@@ -45,5 +56,24 @@ class FrenchElectionTest {
                                                         a3, a2),
                                         getLinearPref(Voter.createVoter(1), a2,
                                                         a3, a1))));
+    }
+
+    @Test
+    void testAlternatives() throws EmptySetException, DuplicateValueException {
+        FrenchElection toTest = FrenchElection.asFrenchElection(ImmutableSet.of(
+                        getLinearPref(Voter.createVoter(1), a1),
+                        getLinearPref(Voter.createVoter(2), a4, a3),
+                        getLinearPref(Voter.createVoter(3), a3, a1, a2)));
+        assertEquals(ImmutableSet.of(a1, a2, a3, a4), toTest.getAlternatives());
+    }
+
+    @Test
+    void testVoters() throws EmptySetException, DuplicateValueException {
+        FrenchElection toTest = FrenchElection.asFrenchElection(ImmutableSet.of(
+                        getLinearPref(Voter.createVoter(1), a1),
+                        getLinearPref(Voter.createVoter(2), a4, a3),
+                        getLinearPref(Voter.createVoter(3), a3, a1, a2)));
+        assertEquals(ImmutableSet.of(Voter.createVoter(2), Voter.createVoter(1),
+                        Voter.createVoter(3)), toTest.getVoters());
     }
 }
