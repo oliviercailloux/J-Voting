387ee973ec626fb3ec14911a114d90cb96de3f47
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-03-19 22:37:19 +0100

    Updt interfaces docs

--- src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/ImmutablePreference.java
+++ src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/ImmutablePreference.java
@@ -7,23 +7,31 @@ import io.github.oliviercailloux.j_voting.Alternative;
 
 /**
  * An Immutable Preference is a preference that cannot be modified
+ *
+ * ImmutablePreference can’t represent the case where we know that a ≥ b and
+ * ignore whether b ≥ a. This class is useful when "what we know so far" can be
+ * represented using ≥ only; in contrast to needing both ≥ and > (as in: I know
+ * that a > b and know that b ≥ c but ignore whether b > c). To put it
+ * otherwise, what I ignore must be symmetric.
+ *
+ * Two immutable preferences are equal iff their graphs are equal.
+ *
  */
 public interface ImmutablePreference extends Preference {
-    
-    /**
-     * {@inheritDoc}
-     *
-     * This graph is immutable.
-     *
-     * @return the Graph corresponding to the Preference.
-     */
-    @Override
-    ImmutableGraph<Alternative> asGraph();
-    
-    /**
-     * {@inheritDoc}
-     * This set is immutable.
-     */
-    @Override
-    ImmutableSet<Alternative> getAlternatives();
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * This graph is immutable.
+	 *
+	 * @return the Graph corresponding to the Preference.
+	 */
+	@Override
+	ImmutableGraph<Alternative> asGraph();
+
+	/**
+	 * {@inheritDoc} This set is immutable.
+	 */
+	@Override
+	ImmutableSet<Alternative> getAlternatives();
 }
--- src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/MutableAntiSymmetricPreference.java
+++ src/main/java/io/github/oliviercailloux/j_voting/preferences/interfaces/MutableAntiSymmetricPreference.java
@@ -8,35 +8,37 @@ import io.github.oliviercailloux.j_voting.Alternative;
  * A mutable antisymmetric preference is an antisymmetric preference (without
  * ex-aequo alternatives) in which some alternatives and edges can be added.
  */
-public interface MutableAntiSymmetricPreference
-                extends AntiSymmetricPreference {
+public interface MutableAntiSymmetricPreference extends AntiSymmetricPreference {
 
-    /**
-     * This graph is not necessarily transitively closed and not necessarily
-     * reflexive
-     *
-     * @see MutableAntiSymmetricPreference#asGraph() TODO : should forbid
-     *      removing alternative from the set of nodes (surprising results when
-     *      a > b > c and removing b as it might disconnect the graph)
-     */
-    public MutableGraph<Alternative> asMutableGraph();
+	/**
+	 * This graph is not necessarily transitively closed and not necessarily
+	 * reflexive. This method might refuse that an edge be added that would result
+	 * in the transitive closure violating anti-symmetry, though it will not
+	 * necessarily check (but it is guaranteed to check if asGraph() has been called
+	 * previously). If such a situation happens, and is not detected (thus implying
+	 * that asGraph() has never been called), an exception will be raised the first
+	 * time asGraph() will be called, unless possibly the transitive closure no more
+	 * violates anti-symmetry at that time (behavior is undefined in such a case).
+	 *
+	 */
+	public MutableGraph<Alternative> asMutableGraph();
 
-    /**
-     * Adds an alternative to the Preference. This alternative is not preferred
-     * to any other of the preference, it is being added isolated.
-     *
-     * @param alternative to add to the preference.
-     */
-    public void addAlternative(Alternative a);
+	/**
+	 * Adds an alternative to the Preference. This alternative is not preferred to
+	 * any other of the preference, it is being added isolated.
+	 *
+	 * @param alternative to add to the preference.
+	 */
+	public void addAlternative(Alternative a);
 
-    /**
-     * Adds an edge from a1 to a2, so that a1 is preferred to a2 (a1 > a2). If
-     * one of them is not in the graph, they are added.
-     *
-     * Graph is rearranged : a transitive closure is applied to it
-     *
-     * @param a1 preferred alternative to a2
-     * @param a2 "lower" alternative
-     */
-    public void addStrictPreference(Alternative a1, Alternative a2);
+	/**
+	 * Adds an edge from a1 to a2, so that a1 is preferred to a2 (a1 > a2). If one
+	 * of them is not in the graph, they are added.
+	 *
+	 * Graph is rearranged : a transitive closure is applied to it
+	 *
+	 * @param a1 preferred alternative to a2
+	 * @param a2 "lower" alternative
+	 */
+	public void addStrictPreference(Alternative a1, Alternative a2);
 }
