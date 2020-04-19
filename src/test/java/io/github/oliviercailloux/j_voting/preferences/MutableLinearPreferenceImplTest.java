d7566175dec0f5eb8bcae413bc98461cc62001ac
PierreRochedy <pierre.rochedy1@gmail.com>
2020-04-19 17:05:08 +0200

    tests

--- src/test/java/io/github/oliviercailloux/j_voting/preferences/MutableLinearPreferenceImplTest.java
+++ src/test/java/io/github/oliviercailloux/j_voting/preferences/MutableLinearPreferenceImplTest.java
@@ -1,6 +1,7 @@
 package io.github.oliviercailloux.j_voting.preferences;
 
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a1;
+import static io.github.oliviercailloux.j_voting.AlternativeHelper.a12345;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a2;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a3;
 import static io.github.oliviercailloux.j_voting.AlternativeHelper.a4;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.Graphs;
 import com.google.common.graph.MutableGraph;
@@ -22,34 +24,45 @@ import io.github.oliviercailloux.j_voting.preferences.interfaces.MutablePreferen
 
 public class MutableLinearPreferenceImplTest {
 	
+	
+	
+	@Test
+    void testGiven() {
+		//Test du given mais pas obligatoire je pense 
+	}
+	/**
+     * Tests whether the preference is correctly expressed as a graph
+     */
 	@Test
     void testAsGraph() {
         MutableGraph<Alternative> graph = GraphBuilder.directed()
-                        .allowsSelfLoops(false).build();
+                        .allowsSelfLoops(true).build();
         graph.putEdge(a1, a2);
-        graph.putEdge(a3, a4);
-        graph.putEdge(a4, a5);
-        graph.putEdge(a5, a3);
+        graph.putEdge(a2, a3);
         MutableLinearPreference pref = MutableLinearPreferenceImpl.given(Voter.createVoter(1), Graphs.copyOf(graph));
-        graph.putEdge(a3, a5);
-        graph.putEdge(a4, a3);
-        graph.putEdge(a5, a4);
+        
+        graph.putEdge(a1,a1);
+        graph.putEdge(a2,a2);
+        graph.putEdge(a3,a3);
+        
+        graph.putEdge(a1,a3);
+        
         assertEquals(graph, pref.asGraph());
     }
 	
-
 	/**
      * Tests whether the single alternative is added to the preferences' graph
      */
     @Test
     void testAddAlternative() {
         MutableGraph<Alternative> graph = GraphBuilder.directed()
-                        .allowsSelfLoops(false).build();
+                        .allowsSelfLoops(true).build();
         graph.putEdge(a1, a2);
-        
         MutableLinearPreference pref = MutableLinearPreferenceImpl.given(Voter.createVoter(1), Graphs.copyOf(graph));
-//        graph.putEdge(a3, a1);
-//        pref.addAlternative(a3);
+        graph.putEdge(a1, a1);
+        graph.putEdge(a2, a2);
+        graph.putEdge(a3, a3);
+        pref.addAlternative(a3);
         assertEquals(graph, pref.asGraph());
     }
     
@@ -62,14 +75,35 @@ public class MutableLinearPreferenceImplTest {
                         .allowsSelfLoops(true).build();
         
         graph.putEdge(a1, a2);
-        graph.putEdge(a2, a1);
-        
         MutableLinearPreference pref = MutableLinearPreferenceImpl.given(Voter.createVoter(1), Graphs.copyOf(graph));
         graph.putEdge(a1, a1);
         graph.putEdge(a2, a2);
         pref.addAlternative(a3);
-        
         pref.deleteAlternative(a3);
         assertEquals(graph, pref.asGraph());
+    }
+	
+	@Test
+    void testChangeOrder() {
+	
+		//a toi de jouer Léo
+		
+		
+	}
+	
+	 /**
+     * Tests whether method getAlternatives returns a set with all the
+     * alternatives of the preference
+     */
+    @Test
+    void testGetAlternatives() {
+        MutableGraph<Alternative> graph = GraphBuilder.directed()
+                        .allowsSelfLoops(true).build();
+        graph.putEdge(a1, a2);
+        graph.putEdge(a3, a4);
+        graph.putEdge(a5, a5);
+        MutableLinearPreference pref = MutableLinearPreferenceImpl.given(Voter.createVoter(1), Graphs.copyOf(graph));
+        ImmutableSet<Alternative> expected = a12345;
+        assertEquals(expected, pref.getAlternatives());
     }
 }
