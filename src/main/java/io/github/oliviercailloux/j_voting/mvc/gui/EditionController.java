8afa97a92d8ede506e70690ab31512cd3d7421be
Julienchilhagopian <julienchilhagopian@gmail.com>
2020-05-29 00:23:17 +0200

    trying to handle event

--- src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/EditionController.java
+++ src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/EditionController.java
@@ -2,9 +2,14 @@ package io.github.oliviercailloux.j_voting.mvc.gui;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import org.eclipse.swt.widgets.*;
 
 import io.github.oliviercailloux.j_voting.Alternative;
+
+import javax.naming.ldap.Control;
 
 public class EditionController {
     private EditionView editionView;
@@ -27,7 +32,18 @@ public class EditionController {
 
         Set<Alternative> a = this.controller.getModel().getAlternatives();
         editionView.displayAlternatives(a);
+        initEventTest();
     }
+
+
+    private void initEventTest() {
+        List test = Arrays.asList(this.editionView.getComposite().getChildren());
+        // Je dois continuer ce test
+        // On recupère tous les élément du composite, faut trouver un moyen de les distinguer
+        // Des qu'on est capable de les distinguer on peut dispatcher  les events correspondants.
+    }
+
+
 
 
 }
--- src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/EditionView.java
+++ src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/EditionView.java
@@ -1,57 +1,55 @@
 package io.github.oliviercailloux.j_voting.mvc.gui;
 
-import com.google.common.base.Preconditions;
 import io.github.oliviercailloux.j_voting.Alternative;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.*;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 public class EditionView {
     private TabFolder tabfolder;
     private TabItem editionTab;
-    private Group group;
-    
-   
+    private Composite mainComposite;
+
+    // Pierre je suis repassé à un composite principal mais ca revient au même que group
+    // .
     public static EditionView create(TabFolder mainTabFolder) {
         return new EditionView(mainTabFolder);
     }
 
     private EditionView(TabFolder mainTabFolder) {
         this.tabfolder = mainTabFolder;
-        this.group = new Group(tabfolder, SWT.NONE);
+        this.mainComposite = new Composite(tabfolder, SWT.NONE);
         initEditionTab();
     }
 
-    // ca va évoluer ça (peut etre passer dans le controller)
+    // Demander si ca passe dans le controller
     private void initEditionTab() {
         this.editionTab = new TabItem(this.tabfolder, SWT.NONE);
         editionTab.setText("Edition");
     }
     
     public void displayVoters(String voterName) {
-    	Text voter = new Text(group, SWT.BORDER);
+    	Text voter = new Text(mainComposite, SWT.BORDER);
     	voter.setText(voterName);
     	voter.setBounds(10,10,200,25);
-    	editionTab.setControl(group);
+    	editionTab.setControl(mainComposite);
     }
     
     public void displayAlternatives(Set<Alternative> altSet) {
-    
-    	// J'ai du créer un group en attribut plutôt qu'un composite et 
-    	// dans le constructeur je lui donne le tabfolder
     	int counterY = 50;
     	for(Alternative a : altSet) {
-            Text alt = new Text(group, SWT.BORDER);
+            Text alt = new Text(mainComposite, SWT.BORDER);
             alt.setText(a.toString());
             alt.setBounds(10,counterY,100,25);
-            editionTab.setControl(group);
+            editionTab.setControl(mainComposite);
             counterY += 30;
         }
 
+    }
 
+    public Composite getComposite() {
+        return this.mainComposite;
     }
 
 	
