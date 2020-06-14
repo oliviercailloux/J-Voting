22dc1905da9e6c5b290adc1a60ad3e39334a2192
Julienchilhagopian <julienchilhagopian@gmail.com>
2020-06-14 19:10:42 +0200

    model rework

--- src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/MainGUI.java
+++ src/main/java/io/github/oliviercailloux/j_voting/mvc/gui/MainGUI.java
@@ -2,14 +2,6 @@ package io.github.oliviercailloux.j_voting.mvc.gui;
 
 import org.eclipse.swt.widgets.*;
 
-import io.github.oliviercailloux.j_voting.Alternative;
-import io.github.oliviercailloux.j_voting.Voter;
-import io.github.oliviercailloux.j_voting.preferences.classes.MutableLinearPreferenceImpl;
-import io.github.oliviercailloux.j_voting.preferences.interfaces.MutableLinearPreference;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.Rectangle;
 
@@ -31,21 +23,8 @@ public class MainGUI {
 		EditionView editionView = view.buildEditionView();
 		VisualizationView visualizationView = view.buildVisualizationView();
 
-		// Model
-		Voter v1 = Voter.createVoter(1);
-		Alternative a = Alternative.withId(2);
-		Alternative a2 = Alternative.withId(3);
-		Alternative a3 = Alternative.withId(4);
-		Alternative a4 = Alternative.withId(5);
-		List<Alternative> alt = new ArrayList<>();
-		alt.add(a);
-		alt.add(a2);
-		alt.add(a3);
-		alt.add(a4);
-		MutableLinearPreference model = MutableLinearPreferenceImpl.given(v1,alt);
-
 		// Controllers
-		Controller controller = Controller.inst(model);
+		Controller controller = Controller.withDefaultModel();
 		EditionController editionController = controller.buildEditionController(editionView);
 
 		shell.open();
@@ -62,7 +41,4 @@ public class MainGUI {
 		shell.setLocation((screenSize.width - shell.getBounds().width) / 2,
 				(screenSize.height - shell.getBounds().height) / 2);
 	}
-
-
-
 }
